72ac1e436b694f12ea8697fc9f5291fd712508b0
Animesh <animesh.sinha@joshsoftware.com>
2024-08-01 16:24:17 +0530

    modified function for fixed range date

--- date_generation.py
+++ date_generation.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta
 
 
-def generate_dates(start_date_str):
+def generate_dates(start_date_str, end_date_str=''):
     start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
     current_date = start_date
-    end_date = start_date + timedelta(days=4 * 365)
+    if end_date_str == '':
+        end_date = start_date + timedelta(days=4 * 365)
+    else:
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
 
     while current_date <= end_date:
         yield current_date.strftime('%Y-%m-%d')
@@ -12,6 +15,14 @@ def generate_dates(start_date_str):
 
 
 start_date = '2024-08-02'
+end_date = '2025-01-01'
+
+# Date generation for given range
+range_date_generator = generate_dates(start_date, end_date)
+for i in range_date_generator:
+    print(i)
+
+# Date generation for 4 years
 date_generator = generate_dates(start_date)
 for i in date_generator:
     print(i)
